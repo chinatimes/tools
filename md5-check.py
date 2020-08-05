ce8ffdd9c1e17a554ead62a330783d823a40230e
chinatimes <lhl_hb@163.com>
2020-08-05 11:06:40 +0800

    md5-check

---
+++ md5-check.py
@@ -0,0 +1,22 @@
+#输出当前目录下文件的MD5
+#chinatimes
+#mail:lhl_hb@163.com
+#20200731
+
+import os
+import hashlib
+
+
+md5log = open('md5log.txt','w');
+for i in  (os.listdir()):
+    if os.path.isfile(i):
+        with open(i, 'rb') as fp:
+            data = fp.read()
+        file_md5= hashlib.md5(data).hexdigest()
+#        print(f'{i:50} ===>  {file_md5}') 
+        print( "%50s ===>: %s"  % (i,file_md5))
+        md5log.write("%-50s ===>: %s\n"  % (i,file_md5)) 
+        
+        
+md5log.close()        
+ 
